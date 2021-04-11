da9b83f05fd8f438bd57289d8ec6709b41cd9841
VtrGon <vhfgoncalves@sga.pucminas.br>
2021-04-11 14:03:21 -0300

    somatorio

--- recursividade.c
+++ recursividade.c
@@ -7,9 +7,25 @@ int fibonacci(int n) {
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
+int somatorio(int n) {
+    int soma = n;
+
+    if(n > 0) soma += somatorio(n - 1);
+
+    return soma;
+}
+
 int main() {
-    for(int i = 0; i < 10; i++)
+    int fib = 10;
+    int som = 5;
+
+    /* Sequencia de fibonacci */
+    printf("\nSequencia de fibonacci de %d posicoes\n", fib);
+    for(int i = 0; i < fib; i++)
         printf("%d ", fibonacci(i));
+
+    /* Somatorio de som*/
+    printf("\n\nSomatorio de %d\n%d",som, somatorio(som));
 
     return 0;
 }
