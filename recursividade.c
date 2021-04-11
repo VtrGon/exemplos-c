2b5da878bfdb1d4e229f8db0e98032f82eebadc4
VtrGon <vhfgoncalves@sga.pucminas.br>
2021-04-11 02:09:26 -0300

    recursividade

---
+++ recursividade.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci(int n) {
+    if(n == 0 || n == 1) return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+    for(int i = 0; i < 10; i++)
+        printf("%d ", fibonacci(i));
+
+    return 0;
+}
